56d121d7d09d00f80343b520f7cc4940511cf305
davilopess <davi.lopes.amorim2014@hotmail.com>
2020-11-10 14:41:01 -0300

    Adicionando mais registros no seed Pedido

--- database/seeds/PedidoTableSeeder.php
+++ database/seeds/PedidoTableSeeder.php
@@ -25,13 +25,41 @@ class PedidoTableSeeder extends Seeder
             status_pedido, data_pedido)
             values (?,?,?,?,?,?,?)',
             array('Djair', 'Dimensao', 'Cozidao','Arroz Branco/Macarrao/Salada',
-            'Dinheiro', false, '2020-11-09'));
+            'Mensalista', false, '2020-11-09'));
 
             DB::insert('insert into pedidos
             (nome_pedido,local_pedido, prato_pedido, acompanhamento_pedido, pagamento_pedido,
             status_pedido, data_pedido)
             values (?,?,?,?,?,?,?)',
             array('Edna', 'Dimensao', 'Macarronada','',
+            'Dinheiro', false, '2020-11-09'));
+
+            DB::insert('insert into pedidos
+            (nome_pedido,local_pedido, prato_pedido, acompanhamento_pedido, pagamento_pedido,
+            status_pedido, data_pedido)
+            values (?,?,?,?,?,?,?)',
+            array('Geanderson', 'Dimensao', 'Carne acebolada','Pure de batata/Arroz Branco/ Salada verde',
+            'Mensalista', false, '2020-11-09'));
+
+            DB::insert('insert into pedidos
+            (nome_pedido,local_pedido, prato_pedido, acompanhamento_pedido, pagamento_pedido,
+            status_pedido, data_pedido)
+            values (?,?,?,?,?,?,?)',
+            array('Daysa', 'Dimensao', 'Panqueca','Pure de batata/Arroz Branco/ Salada verde',
+            'Dinheiro', false, '2020-11-09'));
+
+            DB::insert('insert into pedidos
+            (nome_pedido,local_pedido, prato_pedido, acompanhamento_pedido, pagamento_pedido,
+            status_pedido, data_pedido)
+            values (?,?,?,?,?,?,?)',
+            array('Isaac', 'Dimensao', 'Peixe','Pure de batata/Salada verde',
+            'Dinheiro', false, '2020-11-09'));
+
+            DB::insert('insert into pedidos
+            (nome_pedido,local_pedido, prato_pedido, acompanhamento_pedido, pagamento_pedido,
+            status_pedido, data_pedido)
+            values (?,?,?,?,?,?,?)',
+            array('Yan', 'Dimensao', 'Coxa de frango','Macarrao/Salada verde',
             'Dinheiro', false, '2020-11-09'));
     }
 }
--- resources/views/relatorioMensal.blade.php
+++ resources/views/relatorioMensal.blade.php
@@ -1,43 +1,36 @@
 <h1>Pedidos do mês de Novembro</h1>
     <table class="table table-striped table-hover">
-    <tr>
-            <th>Nome</th>
-            <th>Numero de Pedidos</th>
-            <th>Local</th>
-            <th>Pagamento</th>
+      <tr>
+          <th>Nome</th>
+          <th>Numero de Pedidos</th>
+          <th>Local</th>
+          <th>Pagamento</th>
 
-        </tr>
-        @foreach ($pedidos as $p)
-        <tr  class="">
-            <td>{{$p->nome_pedido}}</td>
-            <td>{{$p->numero_pedido}}</td>
-            <td>{{$p->local_pedido}}</td>
-            <td>{{$p->pagamento_pedido}}</td>
-
-
-            <!-- <td>
-                <a href="">
-                    <span class="material-icons">edit</span>
-                </a>
-            </td> -->
-            <td>
-
-        @endforeach
+      </tr>
+      @foreach ($pedidos as $p)
+      <tr>
+          <td>{{$p->nome_pedido}}</td>
+          <td>{{$p->numero_pedido}}</td>
+          <td>{{$p->local_pedido}}</td>
+          <td>{{$p->pagamento_pedido}}</td>
+          <td>
+      </tr>
+      @endforeach
     </table>
-    <style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+<style>
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
+  td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
 </style>
