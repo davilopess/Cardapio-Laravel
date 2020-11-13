fc1183b744637a7c2150984b1ca69f5f6d07366a
davilopess <davi.lopes.amorim2014@hotmail.com>
2020-11-13 10:33:07 -0300

    Formulario do pedido com imagem

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -7,15 +7,16 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-</head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,200&display=swap" rel="stylesheet">
 <body>
-    <div class="container-pedido">
+    <div class="pedido">
+
         <div class= "container-header">
-            <h1>Solicitação de Almoço</h1>
+            <h1 style="font-family: 'Nunito', sans-serif;
+                ">Solicitação de Almoço</h1>
             <span>Pedido referente a data 12/11/2020</span>
         </div>
-
         <form action="">
             <section>
                 <span>Empresa</span>
@@ -65,7 +66,7 @@
             </section>
             <section>
                 <span>Forma de Pagamento</span>
-                <div container-checkbox>
+                <div check container-checkbox>
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" id="customCheck5">
                         <label class="custom-control-label" for="customCheck5">Dimensão</label>
@@ -76,9 +77,11 @@
                     </div>
                 </div>
             </section>
+            <hr style="border-top: 1px solid rgb(255 255 255 /510%);"/>
+            <button type="button" class="btn btn-outline-light w-25">Enviar</button>
         </form>
     </div>
-        <!-- <img src="/imagens/background.png" alt=""> -->
+    <img style="width: 500px; height: 500px; position: fixed; margin-top: 110px; right: 10px " src="/imagens/cozinha.png" alt="">
 </body>
 <style>
     body {
@@ -86,14 +89,21 @@
         
         font-family: 'Roboto', sans-serif;
         color: white;
+        display: flex;
+        justify-content: space-between;
         
         
     }
-    .container-pedido{
-        margin: 50px 120px;
+    .pedido{
+        margin: 30px 100px;
         display: flex;
-        
+        padding: 20px;
+    
+        box-shadow: 0 0 100px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
         flex-direction: column;
+        width: 700px;
+        /* align-items: center; */
     }
     .container-header{
         margin-bottom: 50px;
@@ -102,20 +112,32 @@
         flex-direction: column;
         
     }
-    .container-checkbox {
+    .check {
         margin-right: 500px;
         display: flex;
         flex-direction: row;
+        width: 300px
+    }
+    
+    /* form{ 
+        padding: 20px;
+    
+        box-shadow: 0 0 100px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+    } */
+    section{
         
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: start; 
+        margin-left: 120px;
+        margin-bottom: 20px;
     }
 
-    section{
-        width: 60%;
-        display: flex;
-        
-        justify-content: space-between;
-        
-        margin-bottom: 20px;
+    section span{
+        font-weight: bold;
+        margin-bottom: 5px;
     }
 </style>
 </html>
